8781787fdf394212b9c2b5246d2418507fb2eb09
ks960037 <ks960037@gmail.com>
2024-01-24 14:31:25 +0800

    修正路徑引用

--- layouts/profile-layout.vue
+++ layouts/profile-layout.vue
@@ -1,12 +1,9 @@
 <template>
   <div>
-    <div
-      class="position-absolute w-100 min-height-300 top-0"
-      :style="{
-        'background-image': 'url(' + image + ')',
-        'background-position-y': '50%',
-      }"
-    >
+    <div class="position-absolute w-100 min-height-300 top-0" :style="{
+      'background-image': 'url(' + image + ')',
+      'background-position-y': '50%',
+    }">
       <span class="mask bg-success opacity-6"></span>
     </div>
     <Sidenav />
@@ -19,8 +16,6 @@
 </template>
 <script setup>
 import image from "@/assets/img/profile-layout-header.jpg";
-import Sidenav from "~~/examples/Sidenav";
-import Footer from "~~/examples/Footer";
 import setNavPills from "~~/assets/js/nav-pills";
 
 onMounted(() => {
--- layouts/vr-layout.vue
+++ layouts/vr-layout.vue
@@ -3,13 +3,10 @@
     <div class="mt-4">
       <Navbar class="bg-success" />
     </div>
-    <div
-      class="mx-3 mt-3 border-radius-xl position-relative"
-      :style="{
-        backgroundImage: `url(${bg})`,
-        backgroundSize: 'cover',
-      }"
-    >
+    <div class="mx-3 mt-3 border-radius-xl position-relative" :style="{
+      backgroundImage: `url(${bg})`,
+      backgroundSize: 'cover',
+    }">
       <Sidenav />
 
       <main class="mt-1 main-content border-radius-lg">
@@ -21,10 +18,6 @@
   </div>
 </template>
 <script setup>
-import Navbar from "@/examples/Navbar";
-import Sidenav from "@/examples/Sidenav";
-import Footer from "@/examples/Footer";
-import Configurator from "~~/examples/Configurator";
 import setTooltip from "@/assets/js/tooltip.js";
 
 import bg from "@/assets/img/vr-bg.jpg";
